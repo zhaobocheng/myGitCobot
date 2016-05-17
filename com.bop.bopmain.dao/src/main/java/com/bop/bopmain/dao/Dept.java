cb138a891b3c4de1e278db85339a15c35b525314
lihui <lihui@BDSOFT>
2016-05-17 06:50:10 +0000

    随机

---
+++ com.bop.bopmain.dao/src/main/java/com/bop/bopmain/dao/Dept.java
@@ -0,0 +1,40 @@
+package com.bop.bopmain.dao;
+
+public class Dept {
+	private String deptId;
+	private String name;
+	private String description;
+	private Integer index;
+	private String parentId;
+	public String getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(String deptId) {
+		this.deptId = deptId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Integer getIndex() {
+		return index;
+	}
+	public void setIndex(Integer index) {
+		this.index = index;
+	}
+	public String getParentId() {
+		return parentId;
+	}
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+	}
+	
+}
