a4a46a39659de82624da8e970815f7cafdc48d85
zhaoxy@BDSOFT <zhaoxy@BDSOFT@df5f3a09-521d-4c42-b8ef-b7b0c85c63bb>
2016-08-12 02:07:13 +0000

    方案浏览添加

--- project/zjj/2-src/com.bop.web.ssj/src/main/java/com/bop/web/ssj/ssjscheme/SchemeResult.java
+++ project/zjj/2-src/com.bop.web.ssj/src/main/java/com/bop/web/ssj/ssjscheme/SchemeResult.java
@@ -1,5 +1,6 @@
 package com.bop.web.ssj.ssjscheme;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -39,7 +40,117 @@ public class SchemeResult {
 	public void setRecordDao(IRecordDao recordDao) {
 		this.recordDao = recordDao;
 	}
+	/**
+	 * 获取方案浏览列表数据
+	 * @param 年份
+	 * @return
+	 */
+	@Action
+	public String getFALBData(String zfnd){ 
 
+		ExtObjectCollection eoc = new ExtObjectCollection();
+		String sql = "select  t.plan00,tt.recordid,tt.parentid,t.plan0101 as nf,t.plan0107 as mc,aa.caption as qx,t.plan0101||t.plan0102 as zfyf,counorg.qys as cycczs , case when p6.plan0602 is null then round(counorg.qys*0.01)  else p6.plan0602  end as ccqys , "+
+					 " counp2.zrs as zfryzs,  counp2.cqrs as cyzfrs,fqs as fqfas,case when tt.plan0302 ='5' then '是' else '否' end as cz from plan01 t  "+
+					 " inner join plan03 tt on tt.parentid = t.plan00 "+				   
+					" left join dm_codetable_data aa on aa.cid=tt.plan0301 and aa.codetablename='DB064' " +
+					 " left join (select count(*) qys,org.reg_district_dic from org01 org group by org.reg_district_dic) counorg on counorg.reg_district_dic = tt.plan0301"+
+					" left join (select count(*) zrs ,sum(decode(p2.plan0204,2,1,0)) as cqrs, p2.plan0205,p2.parentid from plan02 p2 group by p2.plan0205 ,p2.parentid) counp2 on counp2.parentid = t.plan00 and counp2.plan0205=tt.plan0301 "+
+					" left join (select count(*) fqs , p4.plan2103,p4.parentid from plan21 p4 group by p4.plan2103 ,p4.parentid) countorg on countorg.parentid=t.plan00 and countorg.plan2103=tt.plan0301 " + 
+					" left join  plan06 p6  on p6.parentid = t.plan00 and p6.plan0601 = tt.plan0301 where ";
+		
+		String sql1="select plan00,plan00 as recordid,null as parentid,plan0101 as nf,plan0107 as mc,'' as qx,plan0101||plan0102 as zfyf,null as cycczs , null as ccqys , null as zfryzs,  null as cyzfrs,null as fqfas,'' as cz from plan01 where plan0105 = 1  ";
+		
+		String wheresql = " t.plan0105 = 1 ";
+		String zone=this.userSession.getCurrentUserZone();
+		if(zone!=null&&!"".equals(zone)){
+			wheresql += "and tt.plan0301='"+zone+"' ";
+		}
+		if(zfnd!=null&&!"".equals(zfnd)){
+			wheresql += " and t.plan0101 = "+zfnd;
+			sql1+=" and plan0101="+zfnd;
+		}
+		
+		//List<Map<String,Object>> resultList = this.jdbcTemplate.queryForList(sql+wheresql+" order by t.plan0102 desc");
+		List<Map<String,Object>> resultList = this.jdbcTemplate.queryForList(sql+wheresql+" UNION ALL "+sql1+" order by zfyf,parentid desc");
+		SimpleDateFormat  format = new SimpleDateFormat("yyyy-MM-dd");
+		
+		if(resultList.size()>0){
+			for(Map<String,Object> map:resultList){
+				ExtObject eo = new ExtObject();
+				UUID uid = UUID.randomUUID();
+				eo.add("id", map.get("recordid"));
+				eo.add("mc", map.get("mc"));
+				eo.add("nf", map.get("nf"));
+				eo.add("qx", map.get("qx"));
+				eo.add("parentid", map.get("parentid"));
+				eo.add("zfyf", map.get("zfyf"));
+				if (map.get("zfryzs")==null){
+					eo.add("zfryzs","");
+				} else {
+					eo.add("zfryzs",  "<a  id = \"zs\"   Style=\"color:black;\" onclick=\"showRy('zs')\">"+map.get("zfryzs")+"</a>");					
+				}
+				if (map.get("cyzfrs")==null){
+					eo.add("cyzfrs","");
+				}else {
+					eo.add("cyzfrs", "<a  id = \"zfrs\" Style=\"color:black;\" onclick=\"showRy('zf')\">"+map.get("cyzfrs")+"</a>");					
+				}
+				if (map.get("cycczs")==null){
+					eo.add("cycczs","");
+				}else {				
+					eo.add("cycczs",  "<a  id = \"zfrs\" Style=\"color:black;\" onclick=\"showOrg('zs')\">"+map.get("cycczs")+"</a>");
+				}
+				if (map.get("ccqys")==null){
+					eo.add("ccqys","");
+				}else {	
+					eo.add("ccqys", "<a  id = \"zfrs\" Style=\"color:black;\" onclick=\"showOrg('zf')\">"+map.get("ccqys")+"</a>");
+				}
+				eo.add("zffa", map.get("zffa"));
+				eo.add("wtjfas", map.get("wtjfas"));
+				eo.add("cz", map.get("cz"));//是否提交过
+				if (map.get("fqfas")==null){
+					eo.add("fqfas","");
+				}else {				
+					eo.add("fqfas", "<a  id = \"zfrs\" Style=\"color:black;\" onclick=\"showFQScheme('fq')\">"+map.get("fqfas")+"</a>");//废弃方案数
+				}
+				eoc.add(eo);
+			}
+		}
+		return eoc.toString();
+	}
+
+	/**
+	 * 浏览方案时，点击数字下钻，显示废弃方案列表
+	 * @param 
+	 * @return
+	 */
+	@Action
+	public String getFQScheme(String faid){
+		ExtObjectCollection ero = new ExtObjectCollection();
+		String flag=ActionContext.getActionContext().getHttpServletRequest().getParameter("flag");
+		String zone = this.userSession.getCurrentUserZone();
+		String sql = "select a.plan00,t.plan0101,plan0102,t.plan0107,a.plan2101 from plan03 b "+
+					" inner join  plan21 a on a.parentid=b.parentid "+
+					" inner join plan01 t on b.parentid=t.plan00 "+
+					" where b.recordid='"+faid+"' ";
+		
+		if(null!=zone&&!"".equals(zone)){
+			sql +=" and  a.plan2103='"+zone+"'";
+		} 
+
+		List<Map<String,Object>> list = this.jdbcTemplate.queryForList(sql);
+		
+		for(Map<String,Object> map:list){
+			ExtObject eo = new ExtObject();
+			eo.add("id", map.get("plan00"));
+			eo.add("mc", map.get("plan0107"));
+			eo.add("nd", map.get("plan0101"));
+			eo.add("yf", map.get("plan0102"));
+			eo.add("yy", map.get("plan2101"));
+			ero.add(eo);
+		}
+		return ero.toString();		
+		
+	}
 	/**
 	 * 获取代码表值
 	 * @papam 代码表名
---
+++ project/zjj/2-src/com.bop.web.ssj/src/main/resources/com/bop/web/ssj/web_files/ssjScheme/schemeView.jsp
@@ -0,0 +1,129 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<html>
+<head>
+<meta name="decorator" content="miniui">
+<title>执法方案浏览</title>
+</head>
+<body>
+<div style="padding:5px;10px;5px;0">
+	<span>年度：</span><input class="mini-combobox" id="zfnd" style="width:150px;"  onvaluechanged="valueChange" textField="text" valueField="id" url="/ssj/personmanage/personmanage/getCBData/nd?theme=none"/>
+	<a class="mini-button" iconCls = "icon-new" onclick="importExc()" plain="true">导出Excel</a>
+</div>
+<div class="mini-fit">
+	<div id="treegrid1" class="mini-treegrid" style="width:100%;height:100%;"     
+    url="" showTreeIcon="true" treeColumn="zfyf" idField="id" parentField="parentid" resultAsTree="false"  
+    allowResize="false" expandOnLoad="true">
+	    <div property="columns">
+	        <div type="indexcolumn">序号</div>
+	        <div field="id" visible="false">id</div>
+	        <div name="zfyf" field="zfyf" width="60" >任务名称</div>
+	        <div field="qx" width="60">区县</div>
+	        <div field="zfryzs" width="60" align="right">执法人员总数</div>
+	        <div field="cyzfrs" width="60" >参与执法人员数</div>
+	        <div field="cycczs" width="60" >参与抽查企业总数</div>
+	        <div field="ccqys" width="60" >抽查企业数</div>
+	        <div field="cz" width="80" headerAlign="center">是否提交方案 </div>
+	        <div field="fqfas" width="50" headerAlign="center" >废弃方案数</div> 
+	    </div>
+	</div>
+</div>
+
+<script type="text/javascript">
+var Bits=[{id:0,text:'否'},{id:1,text:'是'}];
+
+mini.parse();
+var zfnd = mini.get("zfnd");
+zfnd.select(0);
+
+var grid = mini.get("treegrid1");
+var url = "/ssj/ssjscheme/SchemeResult/getFALBData/" + zfnd.value + "?theme=none";
+grid.setUrl(url);
+//grid.load();
+
+valueChange = function(){
+	var nd = mini.get("zfnd").value;
+	
+	url = "/ssj/ssjscheme/SchemeResult/getFALBData/"+nd+"?theme=none";
+	grid.setUrl(url);
+	grid.reload();
+}
+
+importExc = function(){
+	var faid = mini.get("zfnd").value;
+	grid.loading("正在导出，请稍后......");
+	$.ajax({
+		url:'/ssj/ssjscheme/SchemeResult/exportExcel/'+faid+"?theme=none",
+		type:'get',
+		success:function(e){
+			var inf = mini.decode(e);
+			if(inf.flag){
+				location.href = decodeURI("/ResourceFiles"+inf.path);
+			}else{
+				alert("导出失败咧！");
+			}
+			grid.reload();
+		}
+	});	
+}
+
+showRy = function(e){
+	var faid = grid.getSelected().id;
+	mini.open({
+		url:'/ssj/ssjscheme/showPerson.jsp?theme=2&faid='+faid+"&flag="+e,
+		showMaxButton: false,
+	    allowResize: true,
+	    title: '执法人员情况',
+	    width: 800,
+	    height: 600,
+	    onload: function(){
+	        var iframe = this.getIFrameEl();
+	       // iframe.contentWindow.setData(data);
+	    },
+	    ondestroy: function (action) {
+	    
+	    }	
+	});
+} 
+
+showOrg = function(e){
+	var faid = grid.getSelected().id;
+	mini.open({
+		url:'/ssj/ssjscheme/showOrg.jsp?theme=2&faid='+faid+"&flag="+e,
+		showMaxButton: false,
+	    allowResize: true,
+	    title: '参与企业情况',
+	    width: 800,
+	    height: 600,
+	    onload: function(){
+	        var iframe = this.getIFrameEl();
+	       // iframe.contentWindow.setData(data);
+	    },
+	    ondestroy: function (action) {
+	    
+	    }	
+	});
+} 
+
+showFQScheme = function(e){
+	var faid = grid.getSelected().id;
+	mini.open({
+		url:'/ssj/ssjscheme/showFeiQiScheme.jsp?theme=2&faid='+faid+"&flag="+e,
+		showMaxButton: false,
+	    allowResize: true,
+	    title: '废弃方案',
+	    width: 800,
+	    height: 600,
+	    onload: function(){
+	        var iframe = this.getIFrameEl();
+	       // iframe.contentWindow.setData(data);
+	    },
+	    ondestroy: function (action) {
+	    
+	    }	
+	});
+}
+
+</script>
+</body>
+</html>
