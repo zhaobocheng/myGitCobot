2d4bc423c90914ae75dbd2c828c6a1f53d2bf49f
lihui <lihui@BDSOFT>
2016-09-09 03:51:32 +0000

    统计功能

--- com.bop.web.ssj/src/main/java/com/bop/web/ssj/count/CountPage.java
+++ com.bop.web.ssj/src/main/java/com/bop/web/ssj/count/CountPage.java
@@ -77,17 +77,45 @@ public class CountPage {
 		" left join dm_codetable_data tt on tt.cid = t.plan0404 and tt.codetablename = 'DB064' where t.parentid = '"+id+
 		"' group by t.plan0404,tt.caption  order by t.plan0404 ";
 
+		
+		
+		/*select t.parentid,t.plan1204,dm.caption,ry.ryzs,ry.cyrys,
+		count(*) zqus,
+		sum(decode(t.plan1221,1,1,0)) as qys_uf,
+		sum(decode(t.plan1221,2,1,0)) as qys_fd,
+		sum(decode(t.plan1221,3,1,0)) as qys_ucc,
+		sum(decode(t.plan1221,3,1,0)) as qys_ucc,
+		sum(decode(t.plan1224,1,1,0)) as qys_las,
+		sum(decode(t.plan1226,1,1,0)) as qys_yzx,
+		sum(decode(t.plan1226,2,1,0)) as qys_drrbf,
+		sum(decode(t.plan1226,3,1,0)) as qys_bd,
+		sum(decode(t.plan1226,4,1,0)) as qys_infobf,
+		sum(decode(t.plan1226,5,1,0)) as qys_upro
+		from plan12 t
+		left join dm_codetable_data dm on dm.codetablename = 'DB064' and dm.cid = t.plan1204
+		left join (select p2.parentid,p2.plan0205,count(*) as ryzs,sum(decode(p2.plan0204,2,1,0)) as cyrys from plan02 p2 group by p2.parentid,p2.plan0205) ry on ry.parentid = t.parentid and ry.plan0205 = t.plan1204
+		where t.parentid = '84445d13-b2c2-4440-9eeb-339d0a69a856'
+		group by t.parentid ,t.plan1204,dm.caption,ry.ryzs,ry.cyrys
+*/
+
+
 		ExtObjectCollection exc = new ExtObjectCollection();
 		List<Map<String,Object>> list = this.jdbcTemplate.queryForList(querysql);
 		for(Map<String,Object> map:list){
 			ExtObject eo = new ExtObject();
 			eo.add("qx", map.get("caption"));
-			eo.add("zj", map.get("zj"));
-			eo.add("jl", map.get("jl"));
-			eo.add("ts", map.get("ts"));
-			eo.add("xk", map.get("xk"));
-			eo.add("bz", map.get("bz"));
-			eo.add("cp", map.get("cp"));
+			eo.add("ryzs", map.get("ryzs"));
+			eo.add("cyrys", map.get("cyrys"));
+			eo.add("zqys", map.get("zqys"));
+			
+			eo.add("qys_uf", map.get("qys_uf"));
+			eo.add("qys_fd", map.get("qys_fd"));
+			eo.add("qys_ucc", map.get("qys_ucc"));
+			eo.add("qys_yzx", map.get("qys_yzx"));
+			eo.add("qys_drrbf", map.get("qys_drrbf"));
+			eo.add("qys_bd", map.get("qys_bd"));
+			eo.add("qys_infobf", map.get("qys_infobf"));
+			eo.add("qys_upro", map.get("qys_upro"));
 			exc.add(eo);
 		}
 		return exc.toString();
